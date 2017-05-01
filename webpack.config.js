31db3ecbf9ecc6cb79080cf17261121d0989d16b
Brian O'Toole <brianotoole@Brians-MacBook-Pro.local>
2017-05-01 15:36:38 -0400

    added autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: extractPlugin.extract({
-                    use: ['css-loader', 'sass-loader']
+                    use: ['css-loader', 'sass-loader', 'postcss-loader']
                 })
             }
         ]
